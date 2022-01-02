bc3da3abca54ac98a0a6b1c268c4eecd1909e5fe
nayoonaise <nyyoon3606@gmail.com>
2022-01-02 14:52:26 +0900

    22강 customHook 과제

--- src/UseInputs.js
+++ src/UseInputs.js
@@ -1,4 +1,4 @@
-import {useState, useCallback} from "react";
+import { useCallback, useReducer} from "react";
 
 /*
 * useState대신 useReducer를 사용해서 구현해보기.
@@ -6,16 +6,32 @@ import {useState, useCallback} from "react";
 * 답은 튜토리얼 문서에 있음. https://react.vlpt.us/basic/21-custom-hook.html
 * */
 
-function UseInputs(initialForm) {
-    const [form, setForm] = useState(initialForm);
+function reducer(state, action) {
+    switch (action.type) {
+        case "CHANGE_VALUE":
+            return {...state, [action.name]: action.value};
+        case "RESET_VALUE":
+            return action.value;
+        default:
+            throw new Error("unhandled action");
+    }
+}
 
-    const onChange = useCallback(e => {
-        const {name, value} = e.target;
-        setForm(form => ({...form, [name]:value}));
-    }, [])
+function UseInputs(initialForm) {
+    // const [form, setForm] = useState(initialForm);
+    const [form, dispatch] = useReducer(reducer, initialForm);
+
+    const onChange = useCallback(e => dispatch({
+        type: "CHANGE_VALUE",
+        name: e.target.name,
+        value: e.target.value
+    }), [])
 
     const reset = useCallback(() => {
-        setForm(() => initialForm);
+        dispatch({
+            type: "RESET_VALUE",
+            value: initialForm
+        })
     }, [initialForm])
 
     return [form, onChange, reset];
